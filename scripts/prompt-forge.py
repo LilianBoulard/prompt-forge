866183fe7840be2cc5ac803b899c1d90c6ed2e81
Lilian Boulard <lilian@boulard.fr>
2023-10-02 19:39:07 +0200

    Various fixes and improvements

--- scripts/prompt-forge.py
+++ scripts/prompt-forge.py
@@ -23,8 +23,8 @@ import modules.scripts as scripts
 from modules import errors
 from modules.processing import Processed, process_images
 from modules.shared import state
-from processing import StableDiffusionProcessing
-from prompts_from_file import cmdargs
+from modules.processing import StableDiffusionProcessing
+from scripts.prompts_from_file import cmdargs
 
 
 def is_balanced(parens: str) -> bool:
@@ -90,6 +90,10 @@ def groups_in_group(group: Group | ExclusionGroup) -> list[Group | ExclusionGrou
     return set(groups)
 
 
+class DryRun(BaseException):
+    pass
+
+
 class Candidate:
 
     """
@@ -121,7 +125,7 @@ class Candidate:
             (self.value is not None and self.children)
             or (self.value is None and not self.children)
         ):
-            errors.report(
+            raise ValueError(
                 "Candidate must either be a leaf "
                 "(have a value) or a dummy (has children). " 
                 f"Has both: {self.value=}, {self.children=}"
@@ -302,15 +306,14 @@ class Block:
             candidate = candidate.replace("(", "[").replace(")", " | ]")
             # Avoids unexpected prompts
             if not is_balanced(candidate):
-                errors.report(f"Unbalanced brackets in {candidate!r}")
+                raise ValueError(f"Unbalanced brackets in {candidate!r}")
             candidate_repr = self._construct_parts(candidate)
             candidate_tree = build_candidate_tree(candidate_repr)
             candidates.append(candidate_tree)
 
         if self.weighting == "candidate-shallow":
             # TODO
-            errors.report("`candidate-shallow` is not yet implemented")
-            raise NotImplemented
+            raise NotImplementedError("`candidate-shallow` is not yet implemented")
 
         elif self.weighting == "candidate-deep":
             final_keywords: list[tuple[str, int]] = []
@@ -346,7 +349,7 @@ class Block:
 
         else:
             # FIXME: should be handled by the schema validation
-            errors.report(
+            raise ValueError(
                 f"Expected parameter weighting to be any of "
                 f"{{'candidate-shallow', 'candidate-deep', 'keyword'}}, "
                 f"got {self.weighting!r}. "
@@ -592,7 +595,7 @@ class Generator:
 
     def generate_exhaustive_prompts(self) -> list[str]:
         # TODO
-        raise NotImplemented
+        raise NotImplementedError("mode `exhaustive` is not yet implemented")
 
 
 class Script(scripts.Script):
@@ -604,10 +607,10 @@ class Script(scripts.Script):
     def title(self) -> str:
         return "Prompt forge"
 
-    def show(self, is_txt2img: bool, is_img2img: bool) -> bool:
-        return is_txt2img or is_img2img
+    def show(self, is_img2img: bool) -> bool:
+        return True
 
-    def ui(self) -> tuple[gr.Textbox, gr.Radio, gr.Number]:
+    def ui(self, is_img2img: bool) -> tuple[gr.Textbox, gr.Radio, gr.Number, gr.Checkbox, gr.Checkbox]:
 
         def load_config_file(file: gr.File):
             """
@@ -620,14 +623,8 @@ class Script(scripts.Script):
                 lines = file.decode('utf8', errors='ignore').splitlines()
                 return None, "\n".join(lines), gr.update(lines=7)
 
-        def switch_number_selector(mode_selector: gr.Radio, number_selector: gr.Number) -> None:
-            if mode_selector.value == "Random":
-                number_selector.visible = True
-            elif mode_selector.value == "Exhaustive":
-                number_selector.visible = False
-            else:
-                errors.report(f"Got unexpected generation mode {mode_selector.value!r}")
-
+        GUIDE_LINK = r"https://github.com/LilianBoulard/prompt-forge/blob/main/README.md"
+        gr.HTML(value=f"<br>Confused/new? Read <a style=\"border-bottom: 1px #00ffff dotted;\" href=\"{GUIDE_LINK}\" target=\"_blank\" rel=\"noopener noreferrer\">the guide</a> for usage instructions!<br><br>")
         configuration = gr.Textbox(
             label="Configuration",
 
@@ -640,46 +637,63 @@ class Script(scripts.Script):
             type="binary",
             elem_id=self.elem_id("configuration_file"),
         )
-        generation_type = gr.Radio(
-            label="Mode",
-            info=(
-                "Random will create a number of prompts randomly picked from the configuration, "
-                "exhaustive will generate all possible combinations."
-            ),
-            choices=["Random", "Exhaustive"],
-        )
-        number_of_entries = gr.Number(
-            label="Number of random prompts to generate",
-            precision=0,
-            minimum=0,
-        )
-        warn_if_duplicates = gr.Checkbox(
-            label="Warn when there are duplicate prompts",
-            info="Note: can only happen when using mode 'Random'",
-            value=True,
-        )
+        with gr.Row():
+            generation_type = gr.Radio(
+                label="Mode",
+                info=(
+                    "Random will create a number of prompts randomly picked from the configuration, "
+                    "exhaustive will generate all possible combinations."
+                ),
+                choices=["Random", "Exhaustive"],
+                value="Random",
+            )
+            number_of_entries = gr.Number(
+                label="Number of random prompts to generate",
+                info="Note: ignore if using exhaustive mode",
+                precision=0,
+                minimum=0,
+            )
+        with gr.Row():
+            warn_if_duplicates = gr.Checkbox(
+                label="Warn when there are duplicate prompts (Note: can only happen when using mode 'Random')",
+                value=True,
+            )
+            dry_run = gr.Checkbox(
+                label="Dry run to test the configuration",
+                value=False,
+            )
 
         # Push configuration file's content into the textbox
         configuration_file.change(fn=load_config_file, inputs=[configuration_file], outputs=[configuration_file, configuration, configuration], show_progress=False)
-        # Listen to changes on the mode selector, and enable/disable the entries
+        # TODO: Listen to changes on the mode selector, and enable/disable the entries
         # selector depending on the value
-        generation_type.change(switch_number_selector, inputs=[generation_type, number_of_entries])
+        # TODO: Disable duplicates warning when using mode exhaustive
 
-        return configuration, generation_type, number_of_entries, warn_if_duplicates
+        return (
+            configuration,
+            generation_type,
+            number_of_entries,
+            warn_if_duplicates,
+            dry_run,
+        )
 
     def run(
             self,
             p: StableDiffusionProcessing,
-            configuration: gr.Textbox,
-            generation_type: gr.Radio,
-            number_of_entries: gr.Number,
-            duplicates_warning: gr.Checkbox,
+            configuration: str,
+            generation_type: str,
+            number_of_entries: int,
+            duplicates_warning: bool,
+            dry_run: bool,
         ):
         """
         Parts of this function are from script
         [prompts_from_file](https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/scripts/prompts_from_file.py)
         (https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/5ef669de080814067961f28357256e8fe27544f4/scripts/prompts_from_file.py).
         """
+        if not configuration:
+            raise RuntimeError("Configuration is required, please load one in. Read the script's guide for guidance.")
+
         generator: Generator = Generator.from_string(configuration)
 
         if generation_type == "Random":
@@ -687,13 +701,26 @@ class Script(scripts.Script):
         elif generation_type == "Exhaustive":
             lines = generator.generate_exhaustive_prompts()
         else:
-            errors.report(f"Invalid generation type {generation_type!r}")
+            raise ValueError(f"Invalid generation type {generation_type!r}")
 
         if duplicates_warning:
             n_uniques = len(set(lines))
             duplicates = len(lines) - n_uniques
             if duplicates > 0:
                 errors.report(f"The generated prompts contain duplicates ({duplicates}/{len(lines)})")
+
+        if dry_run:
+            print(f"Prompt-forge dry run: generated {len(lines)} prompts:")
+            if len(lines) <= 8:
+                for line in lines:
+                    print(line)
+            else:
+                for line in lines[:3]:
+                    print(line)
+                print(f"... ({len(lines) - 6} prompts)")
+                for line in lines[-3:]:
+                    print(line)
+            raise RuntimeWarning("Dry run completed without errors. Check the logs for details.")
 
         p.do_not_save_grid = True
 
@@ -705,7 +732,7 @@ class Script(scripts.Script):
                 try:
                     args = cmdargs(line)
                 except Exception:
-                    errors.report(f"Error parsing line {line} as commandline", exc_info=True)
+                    errors.report(f"Error parsing prompt {line} as commandline", exc_info=True)
                     args = {"prompt": line}
             else:
                 args = {"prompt": line}
@@ -714,7 +741,7 @@ class Script(scripts.Script):
 
             jobs.append(args)
 
-        print(f"Will process {len(lines)} lines in {job_count} jobs.")
+        print(f"Will process {len(lines)} prompt in {job_count} jobs.")
         if p.seed == -1:
             p.seed = int(random.randrange(4294967294))
 
